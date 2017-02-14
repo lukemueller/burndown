2f5554e4502cd1b87880d4c5e1bc1a4e77fa06cb
David Julia <dajulia3@gmail.com>
2017-02-13 18:30:09 -0600

    get rid of unneeded comment in BurndownApplication

--- src/main/java/io/pivotal/BurndownApplication.java
+++ src/main/java/io/pivotal/BurndownApplication.java
@@ -13,35 +13,4 @@ public class BurndownApplication {
     public static void main(String[] args) {
         SpringApplication.run(BurndownApplication.class);
     }
-
-//    private static final Integer totalBudget = 697467;
-//    private static List<Integer> weeklyConsumption = new ArrayList<>();
-//    private static Random random = new Random();
-//
-//    public static void main(String[] args) {
-//        while (thereIsStillBudget()) {
-//            int next = random.nextInt(40000);
-//            System.out.println("next = " + next);
-//            weeklyConsumption.add(next);
-//        }
-//
-//        ArrayList<Integer> burndown = new ArrayList<>();
-//        int remainingBudget = totalBudget;
-//        for (Integer datum : weeklyConsumption) {
-//            remainingBudget -= datum;
-//            burndown.add(remainingBudget);
-//        }
-//
-//        System.out.println("burndown = " + burndown);
-//        System.out.println("burndown.size() = " + burndown.size());
-//    }
-//
-//    private static boolean thereIsStillBudget() {
-//        Integer usedBudget = 0;
-//        for (Integer datum : weeklyConsumption) {
-//            usedBudget += datum;
-//        }
-//
-//        return usedBudget <= totalBudget;
-//    }
 }
