46ebb2071caaa7b02b00d9cfa833b80934c013bb
Luke Mueller <luke.jordan.mueller@gmail.com>
2017-06-26 14:38:01 -0500

    Adding /parse API endpoint for SendGrid Parse API

--- src/main/java/io/pivotal/BurndownApplicationConfiguration.java
+++ src/main/java/io/pivotal/BurndownApplicationConfiguration.java
@@ -1,7 +1,19 @@
 package io.pivotal;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class BurndownApplicationConfiguration {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeHeaders(true);
+        return filter;
+    }
 }
