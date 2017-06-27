abaaae20f9efa992bda40839c84d81cb2e79a613
Samuel Waggoner <swaggoner@pivotal.io>
2017-06-27 13:59:29 -0500

    WIP - parse out email from SendGrid Inbound Parse API

--- src/main/java/io/pivotal/parse/ParseController.java
+++ src/main/java/io/pivotal/parse/ParseController.java
@@ -1,29 +1,33 @@
 package io.pivotal.parse;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import au.com.bytecode.opencsv.CSVReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
+import java.io.InputStreamReader;
 
 @RestController
 class ParseController {
 
-    private Logger logger;
-
-    public ParseController() {
-        this.logger = LoggerFactory.getLogger(ParseController.class);
-    }
+    private Logger logger = LoggerFactory.getLogger(ParseController.class);
 
     @PostMapping(value = "/parse")
-    void receiveUpdatedBurn(HttpServletRequest request) throws IOException {
-        InputStream inputStream = request.getInputStream();
-
-        logger.debug("contents", StreamUtils.copyToString(inputStream, Charset.forName("UTF-8")));
+    void receiveUpdatedBurn(HttpServletRequest request,
+                            @RequestPart("subject") String string,
+                            @RequestPart("attachments") String attachmentCount,
+                            @RequestPart("attachment-info") String attachmentInfo
+    ) throws IOException, ServletException {
+        try (CSVReader reader = new CSVReader(new InputStreamReader(request.getPart("attachment1").getInputStream()))) {
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+//                logger.debug(line);
+            }
+        }
     }
 }
