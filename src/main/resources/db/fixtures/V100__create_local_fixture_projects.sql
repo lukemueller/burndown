8e37097ae37e1733badac02f43f153d69cd8cb7e
Luke Mueller <luke.jordan.mueller@gmail.com>
2017-07-19 15:53:12 -0500

    Adding fixture data for local development

---
+++ src/main/resources/db/fixtures/V100__create_local_fixture_projects.sql
@@ -0,0 +1,2 @@
+INSERT INTO projects (name, start_date, hourly_rate, budget) VALUES ("Kitten Mittens", "2017-01-01", 100, 500000);
+INSERT INTO projects (name, start_date, hourly_rate, budget) VALUES ("dicktowels.com", "2016-10-15", 125, 125000);
